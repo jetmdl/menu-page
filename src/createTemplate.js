48939dfedccd261e52492be066461387887fd0c3
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-21 23:46:52 +0100

    added menu to dynamic template file

--- src/createTemplate.js
+++ src/createTemplate.js
@@ -2,11 +2,14 @@ function createTemplate(){
     const leftPad = document.createElement('div');
     const rightPad = document.createElement('div');
     const centreContent = document.createElement('div'); 
+    const menuBar = document.createElement('div');
 
     leftPad.id = 'leftPad';
     rightPad.id = 'rightPad';
     centreContent.id = 'centreContent';
+    menuBar.id = 'menuBar';
 
+    document.querySelector('.content').appendChild(menuBar);
     document.querySelector('.content').appendChild(leftPad);
     document.querySelector('.content').appendChild(centreContent);
     document.querySelector('.content').appendChild(rightPad);
