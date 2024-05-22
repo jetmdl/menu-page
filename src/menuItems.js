de493de1cb8caed9d9262982fec0d93330454887
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-22 23:08:07 +0100

    added array to items

---
+++ src/menuItems.js
@@ -0,0 +1,51 @@
+export default function getMenuItems() {
+    // Pretend this is an API or that gets a SQL list of items or something:
+    const menuItems = [
+        {
+            "dish": "Madras",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Jalfriezi",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Rogon",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Balti",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Bhuna",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Dupiaza",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Sagwali",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Tikka Masala",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "dish": "Jalfriezi",
+            "price": 5.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+    ];
+    return(menuItems);
+}
