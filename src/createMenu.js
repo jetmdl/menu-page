b113de2c8d5bd0555a9698f3faba22c8aebfa218
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-22 23:07:31 +0100

    added menuItems.js

---
+++ src/createMenu.js
@@ -0,0 +1,10 @@
+import getMenuItems from './menuItems';
+
+const items = getMenuItems();
+
+export default function insertItems(){
+    for(i=0; i<items.length; i++){
+
+    }
+}
+
--- src/createTemplate.js
+++ src/createTemplate.js
@@ -1,17 +1,20 @@
 export default function createTemplate(){
+
+    const menuBar = document.createElement('div');
+    const flexContainer = document.createElement('div');
     const leftPad = document.createElement('div');
     const rightPad = document.createElement('div');
     const centreContent = document.createElement('div'); 
-    const menuBar = document.createElement('div');
 
+    flexContainer.id = 'flexContainer';
     leftPad.id = 'leftPad';
     rightPad.id = 'rightPad';
     centreContent.id = 'centreContent';
     menuBar.id = 'menuBar';
 
-    //document.querySelector('.body').appendChild(menuBar);
-    document.querySelector('.content').appendChild(leftPad);
-    document.querySelector('.content').appendChild(centreContent);
-    document.querySelector('.content').appendChild(rightPad);
+    document.querySelector('.content').appendChild(menuBar);
+    document.querySelector('.content').appendChild(flexContainer);
+    document.querySelector('#flexContainer').appendChild(leftPad);
+    document.querySelector('#flexContainer').appendChild(centreContent);
+    document.querySelector('#flexContainer').appendChild(rightPad);
 }
-
