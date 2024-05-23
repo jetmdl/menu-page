ce7dee0ba468a17946df42a8b9860ded3f3ecd4a
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-23 23:01:41 +0100

    added all menu items

--- src/menuItems.js
+++ src/menuItems.js
@@ -2,49 +2,163 @@ export default function getMenuItems() {
     // Pretend this is an API that gets a SQL list of items or something:
     const menuItems = [
         {
+            "type": "heading",
+            "heading": "Starters"
+        },
+        {
+            "type": "dish",
+            "category": "starters",
+            "dish": "Samosa",
+            "price": 2.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "dish",
+            "category": "starters",
+            "dish": "Puree",
+            "price": 2.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "dish",
+            "category": "starters",
+            "dish": "Soup",
+            "price": 2.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "dish",
+            "category": "starters",
+            "dish": "Tikka",
+            "price": 2.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "dish",           
+            "category": "starters",
+            "dish": "Kebab",
+            "price": 2.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "dish",
+            "category": "starters",
+            "dish": "Chat",
+            "price": 2.50,
+            "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "heading",
+            "heading": "Mains"
+        },
+        {
+            "type": "dish",
+            "category": "mains",
             "dish": "Madras",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Jalfriezi",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Rogon",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Balti",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Bhuna",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Dupiaza",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Sagwali",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
+            "type": "dish",
+            "category": "mains",
             "dish": "Tikka Masala",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
         },
         {
-            "dish": "Jalfriezi",
+            "type": "dish",
+            "category": "mains",
+            "dish": "Vindaloo",
             "price": 5.50,
             "variants": ['chicken', 'pork', 'vegetable']
+        },
+        {
+            "type": "heading",
+            "heading": "Sides"
+        },
+        {
+            "type": "dish",
+            "category": "sides",
+            "dish": "Boiled Rice",
+            "price": 2.50,
+            "variants": ['Plain']
+        },
+        {
+            "type": "dish",
+            "category": "sides",
+            "dish": "Pilau Rice",
+            "price": 2.50,
+            "variants": ['Plain']
+        },
+        {
+            "type": "dish",
+            "category": "sides",
+            "dish": "Special Rice",
+            "price": 2.50,
+            "variants": ['Plain']
+        },
+        {
+            "type": "dish",
+            "category": "sides",
+            "dish": "Plain Naan",
+            "price": 2.50,
+            "variants": ['Plain']
+        },
+        {
+            "type": "dish",
+            "category": "sides",
+            "dish": "Garlic Naan",
+            "price": 2.50,
+            "variants": ['Plain']
+        },
+        {
+            "type": "dish",
+            "category": "sides",
+            "dish": "Rhoti",
+            "price": 2.50,
+            "variants": ['Plain']
         },
     ];
     return(menuItems);
