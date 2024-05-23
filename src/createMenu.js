384525f1d4f703987ae3a7e9ab48ed67bbaaf301
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-23 23:14:00 +0100

    added categories to menu

--- src/createMenu.js
+++ src/createMenu.js
@@ -1,13 +1,27 @@
 export default function insertItems(items){
     let idIncrement = 0;
+    let catIncrement = 0;
     console.log(items);
     for(let i=0; i<items.length; i++){
-        const menuItemDiv = document.createElement('div');
-        menuItemDiv.className = 'menuItem';
-        menuItemDiv.id = 'menuItem' + idIncrement;
-        menuItemDiv.innerText = items[i].dish;
+        if(items[i].type == 'dish'){
+            const menuItemDiv = document.createElement('div');
+            menuItemDiv.className = 'menuItem';
+            menuItemDiv.id = 'menuItem' + idIncrement;
+            menuItemDiv.innerText = items[i].dish;
+    
+            document.querySelector('#centreContent').appendChild(menuItemDiv);
+            idIncrement++;
+        }
+        else if (items[i].type == 'heading'){
+            const categoryDiv = document.createElement('div');
+            categoryDiv.className = 'menuCategory';
+            categoryDiv.id = 'menuCategory' + catIncrement;
+            categoryDiv.innerText = items[i].heading;
+    
+            document.querySelector('#centreContent').appendChild(categoryDiv); 
+            catIncrement++;
+        }
 
-        document.querySelector('#centreContent').appendChild(menuItemDiv);
     }
 }
 
