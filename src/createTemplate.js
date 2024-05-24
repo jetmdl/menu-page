7bf9651df0f0539fc55d1de8fa2f54e0ef2fc2f1
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-24 16:06:13 +0100

    reorganised template.js

--- src/createTemplate.js
+++ src/createTemplate.js
@@ -1,20 +1,22 @@
 export default function createTemplate(){
 
     const menuBar = document.createElement('div');
-    const flexContainer = document.createElement('div');
-    const leftPad = document.createElement('div');
-    const rightPad = document.createElement('div');
-    const centreContent = document.createElement('div'); 
-
-    flexContainer.id = 'flexContainer';
-    leftPad.id = 'leftPad';
-    rightPad.id = 'rightPad';
-    centreContent.id = 'centreContent';
     menuBar.id = 'menuBar';
-
     document.querySelector('.content').appendChild(menuBar);
+
+    const flexContainer = document.createElement('div');
+    flexContainer.id = 'flexContainer';
     document.querySelector('.content').appendChild(flexContainer);
+
+    const leftPad = document.createElement('div');
+    leftPad.id = 'leftPad';
     document.querySelector('#flexContainer').appendChild(leftPad);
+
+    const centreContent = document.createElement('div'); 
+    centreContent.id = 'centreContent';
     document.querySelector('#flexContainer').appendChild(centreContent);
+
+    const rightPad = document.createElement('div');
+    rightPad.id = 'rightPad';
     document.querySelector('#flexContainer').appendChild(rightPad);
 }
