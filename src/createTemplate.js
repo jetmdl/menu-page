27de7d72c92991a8fbe2f694c1ba5bbfd7e442d0
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-24 21:01:53 +0100

    added content to menu bar

--- src/createTemplate.js
+++ src/createTemplate.js
@@ -1,21 +1,51 @@
 export default function createTemplate(){
 
+    //Create and append top menu bar 
     const menuBar = document.createElement('div');
     menuBar.id = 'menuBar';
     document.querySelector('.content').appendChild(menuBar);
 
+    //Create and append icon container in menu bar 
+    const menuIconContainer = document.createElement('div');
+    menuIconContainer.id = 'menuIconContainer';
+    document.querySelector('#menuBar').appendChild(menuIconContainer);
+
+    //Create and append button container and button divs in menu bar 
+    const menuButtonContainer = document.createElement('div');
+    menuButtonContainer.id = 'menuButtonContainer';
+    document.querySelector('#menuBar').appendChild(menuButtonContainer);
+
+    const menuHomeButton = document.createElement('div');
+    menuHomeButton.id = 'menuHomeButton';
+    menuHomeButton.innerHTML = 'Home';
+    document.querySelector('#menuButtoncontainer').appendChild(menuHomeButton);
+
+    const menuMenuButton = document.createElement('div');
+    menuMenuButton.id = 'menuMenuButton';
+    menuMenuButton.innerHTML = 'Menu';
+    document.querySelector('#menuButtoncontainer').appendChild(menuMenuButton);
+
+    const menuContactButton = document.createElement('div');
+    menuContactButton.id = 'menuContactButton';
+    menuContactButton.innerHTML = 'Contact';
+    document.querySelector('#menuButtoncontainer').appendChild(menuContactButton);
+
+    //Create and append the flex container that holds the center content and padding divs 
     const flexContainer = document.createElement('div');
     flexContainer.id = 'flexContainer';
-    document.querySelector('.content').appendChild(flexContainer);
+    document.querySelector('#content').appendChild(flexContainer);
 
+    //Create top menu bar 
     const leftPad = document.createElement('div');
     leftPad.id = 'leftPad';
     document.querySelector('#flexContainer').appendChild(leftPad);
 
+    //Create top menu bar 
     const centreContent = document.createElement('div'); 
     centreContent.id = 'centreContent';
     document.querySelector('#flexContainer').appendChild(centreContent);
 
+    //Create top menu bar 
     const rightPad = document.createElement('div');
     rightPad.id = 'rightPad';
     document.querySelector('#flexContainer').appendChild(rightPad);
