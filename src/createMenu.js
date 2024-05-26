d5747760b75661b3a5328b8cac33d2d10ebd7edd
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-26 01:08:38 +0100

    menu items expand/contract when clicked

--- src/createMenu.js
+++ src/createMenu.js
@@ -1,7 +1,6 @@
 export default function insertItems(items){
     let idIncrement = 0;
     let catIncrement = 0;
-    console.log(items);
     for(let i=0; i<items.length; i++){
         if(items[i].type == 'dish'){
             const menuItemDiv = document.createElement('div');
@@ -10,6 +9,7 @@ export default function insertItems(items){
             menuItemDiv.innerText = items[i].dish;
     
             document.querySelector('#centreContent').appendChild(menuItemDiv);
+            menuItemDiv.addEventListener('click', GetClickedId);
             idIncrement++;
         }
         else if (items[i].type == 'heading'){
@@ -23,5 +23,22 @@ export default function insertItems(items){
         }
 
     }
+    function GetClickedId(){
+        expandDiv(this.id);
+    }
+    function expandDiv(anId){
+        const toExpand = document.getElementById(anId);
+        toExpand.removeEventListener('click', GetClickedId);
+        toExpand.style = 'height: 200px';
+        toExpand.addEventListener('click', contractGetClickedId);
+    }
+    function contractGetClickedId(){
+        contractDiv(this.id);
+    }
+    function contractDiv(anId){
+        const toContract = document.getElementById(anId);
+        toContract.removeEventListener('click', contractGetClickedId);
+        toContract.style = 'height: 50px';
+        toContract.addEventListener('click', GetClickedId);
+    }
 }
-
